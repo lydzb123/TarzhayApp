00acdc6ad34173fd0b733137f598934ff495f2ef
Jason Chen <jason.l.chen@gmail.com>
2020-12-01 09:46:45 -0800

    added compression

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require ('express');
+const compression = require('compression');
 const bodyParser = require('body-parser');
 const path = require('path');
 const { Product } = require('../database');
@@ -9,21 +10,12 @@ if (port == null || port == "") {
   port = 3001;
 }
 
+app.use(compression());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(__dirname + '/../client/dist'));
 app.use('/api/:id', express.static(__dirname + '/../client/dist'));
 app.use('/:id', express.static(__dirname + '/../client/dist'));
-
-
-// app.get('/:id', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/../client/dist/index.html'));
-// })
-
-// app.get('/api/:id', (req, res) => {
-//   console.log('GET received' + req.params.id);
-//   res.sendFile(path.join(__dirname + '/../client/dist/index.html'));
-// })
 
 app.get('/api/products/:id', async (req, res) => {
   console.log(`${req.method} received at ${req.url}`);
